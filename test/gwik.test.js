75d56b0ca6fb0a0142d719fdd7cb10ddf4cc58fd
ibra <ibrahimalanshor6@gmail.com>
2023-02-07 11:49:59 +0800

    feat: readme

--- test/gwik.test.js
+++ test/gwik.test.js
@@ -1,22 +1,22 @@
 const { expect } = require('chai');
 const { describe, it } = require('mocha');
 
-describe('serving test', () => {
-  const serving = require('../index');
+describe('gwik test', () => {
+  const gwik = require('../index');
 
   it('shoud be defined', () => {
-    expect(serving).not.to.be.undefined;
-    expect(serving).to.be.a('object');
+    expect(gwik).not.to.be.undefined;
+    expect(gwik).to.be.a('object');
   });
 
   it('should exports server function constructor', () => {
-    expect(serving).to.have.property('Server');
-    expect(serving.Server).to.be.a('function');
+    expect(gwik).to.have.property('Server');
+    expect(gwik.Server).to.be.a('function');
   });
 
   it('should exports router function constructor', () => {
-    expect(serving).to.have.property('Router');
-    expect(serving.Router).to.be.a('function');
+    expect(gwik).to.have.property('Router');
+    expect(gwik.Router).to.be.a('function');
   });
 
   it('should exports all exceptions', () => {
@@ -29,15 +29,15 @@ describe('serving test', () => {
       'Unauthorized',
       'UnprocessableEntity',
     ]) {
-      expect(serving).to.have.property(`${exception}Exception`);
+      expect(gwik).to.have.property(`${exception}Exception`);
     }
   });
 
   it('should exports body validation middleware', () => {
-    expect(serving).to.have.property('createBodyValidationMiddleware');
+    expect(gwik).to.have.property('createBodyValidationMiddleware');
   });
 
   it('should exports multipart form data middleware', () => {
-    expect(serving).to.have.property('createMultipartFormDataMiddleware');
+    expect(gwik).to.have.property('createMultipartFormDataMiddleware');
   });
 });
